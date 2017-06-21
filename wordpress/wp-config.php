5d678b98c930aa28567f7ff0990f2f503bafe29f
Ake KingOfBuffet <ake.zakky@gmail.com>
2017-06-21 05:19:01 +0000

    Update wordpress to 4.8

--- wordpress/wp-config.php
+++ wordpress/wp-config.php
@@ -23,16 +23,16 @@
 define('DB_NAME', 'wpdb');
 
 /** MySQL database username */
-define('DB_USER', 'superwordpress');
+define('DB_USER', 'zakrified909');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'wvlyfvpjkcV8');
+define('DB_PASSWORD', 'vpjkcV8d^ot0Ut555');
 
 /** MySQL hostname */
-define('DB_HOST', 'wordpressdb:3306');
+define('DB_HOST', 'mariadb-master');
 
 /** Database Charset to use in creating database tables. */
-define('DB_CHARSET', 'utf8');
+define('DB_CHARSET', 'utf8mb4');
 
 /** The Database Collate type. Don't change this if in doubt. */
 define('DB_COLLATE', '');
@@ -46,14 +46,14 @@ define('DB_COLLATE', '');
  *
  * @since 2.6.0
  */
-define('AUTH_KEY',         'd7633e718d56d339bd477f208aa21ea94a306b6e');
-define('SECURE_AUTH_KEY',  '13187f194f312cef1c8e75b491e1d85e742ac92b');
-define('LOGGED_IN_KEY',    '5ff7c1d0a731498dc731017a19e8d9cf3dc55620');
-define('NONCE_KEY',        '010549ddc231cd7b4bcedf2d54cb8e29ef5229c3');
-define('AUTH_SALT',        'b2cca683d39376408f88a8cb57384f0d724eec51');
-define('SECURE_AUTH_SALT', '1b8c152f1553859783eace921f919cc2b281d2cf');
-define('LOGGED_IN_SALT',   'd371ce1777276bf83f2f40291bb30e338fa4204e');
-define('NONCE_SALT',       '9f38413392090c44b8d2dc4b8c4b05c1a8ac5a94');
+define('AUTH_KEY',         '0lut0@b{1%Yo}Q9qTkgot{i?uWwEQuxS0vU:VDe;*>Fx,D7ui1SYzbo-j>~xXvvB');
+define('SECURE_AUTH_KEY',  'V:T=n09t&mDJ>~Mi5xb3wtFn,hET]NuZ3a4p+2?FYs#^V~oU(/Bm(z;Zc?m4siFH');
+define('LOGGED_IN_KEY',    '3h3Lg7u!i,!0GywR=:cisWpw!plNERbPA-YS%Sy%IBJym([4n-[PBxEvs302w4(}');
+define('NONCE_KEY',        '6h<pk_Q(/Fcp)Z(f-L._r-a x^H:FJG@*T1UTz]>,Ob;*|ZXS&qq)VA[[}n9M.ig');
+define('AUTH_SALT',        '_kvXm7NkC5PpRHdF r,[Vo`c, lTw^qQGxt2zth`Y5=SkuO-3hd@<b6eU@p@]Y?_');
+define('SECURE_AUTH_SALT', 'm7uIKP0x.mDj%<5g2}Qn-hgcvH3^^xq|MS=N>o%;x,@X[rWyT$+~Vq%r)8]:1lFM');
+define('LOGGED_IN_SALT',   'F^iNl01en3QtM6YV_UlVq^Ba::xA1~&V[*ddeB>7m/C.K8c=QLU);7o;Os=u-Mj/');
+define('NONCE_SALT',       '&Y<jW(%}CiE/!^A-IcK1Zv>0g-! I/#U(5#{`;T[p}=2EY7b,6F0jp95M,aEHegX');
 
 /**#@-*/
 
@@ -78,12 +78,6 @@ $table_prefix  = 'wp_';
  * @link https://codex.wordpress.org/Debugging_in_WordPress
  */
 define('WP_DEBUG', false);
-
-// If we're behind a proxy server and using HTTPS, we need to alert Wordpress of that fact
-// see also http://codex.wordpress.org/Administration_Over_SSL#Using_a_Reverse_Proxy
-if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
-	$_SERVER['HTTPS'] = 'on';
-}
 
 /* That's all, stop editing! Happy blogging. */
 
